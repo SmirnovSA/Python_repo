d9f9b0da27cf37b7d2f39f5cbf6762d08d90cec2
SmirnovSA <ssasml67@gmail.com>
2020-03-12 08:06:04 +0000

    Выполнил минимум заданий раздела 12.x

--- exercises/12_useful_modules/task_12_1.py
+++ exercises/12_useful_modules/task_12_1.py
@@ -14,3 +14,33 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+import subprocess
+def ping_ip(ip_address):
+    reply = subprocess.run(
+        ["ping", "-c", "3", "-n", ip_address],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+    )
+    if reply.returncode == 0:
+        return True, reply.stdout
+    else:
+        return False, reply.stderr
+def ping_ip_addresses(ip_address):
+    result_ok = []
+    result_nok = []
+    total = tuple([result_ok] + [result_nok])
+    for ip in ip_address:
+        success, func_result = ping_ip(ip)
+        if success:
+            result_ok.append(ip)
+        else:
+            result_nok.append(ip)
+    return total
+if __name__ == "__main__":
+    ip_address = ["1.1.1", "8.8.8.8", "8.8.4.4", "8.8.7.1"]
+    print(ping_ip_addresses(ip_address))
+
+
+
+    
--- exercises/12_useful_modules/task_12_2.py
+++ exercises/12_useful_modules/task_12_2.py
@@ -30,3 +30,38 @@
  '172.21.41.129', '172.21.41.130', '172.21.41.131', '172.21.41.132']
 
 """
+
+import ipaddress
+def check_ip_correct(ip):
+    try:
+        ipaddress.ip_address(ip)
+        return True
+    except ValueError as err:
+        return False
+        
+def convert_ranges_to_ip_list(ip_list):
+    result = []
+    for ip in ip_list:
+        if check_ip_correct(ip):
+            result.append(ip)
+        else:
+            ip = ip.split('-')
+            ip_start = ip[0]
+            ip_end = int(ip[-1].split('.')[-1])
+            ip_start = ipaddress.ip_address(ip_start)
+            #ip_end = ipaddress.ip_address(ip_end)
+            result.append(str(ip_start))
+            while int(str(ip_start).split('.')[-1]) < int(ip_end):
+                ip_start = ip_start + 1
+                result.append(str(ip_start))
+    return result
+if __name__ == "__main__":
+    ip_list = ['8.8.4.4', '1.1.1.1-3', '172.21.41.128-172.21.41.132']
+    print(convert_ranges_to_ip_list(ip_list))
+
+
+
+
+        
+
+
