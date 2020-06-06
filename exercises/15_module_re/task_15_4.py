dd07a01e794f5df4ef6e91aa121c74fdb89e58fa
SmirnovSA <ssasml67@gmail.com>
2020-06-06 12:50:52 +0000

    Выполнено задание 17_1

--- exercises/15_module_re/task_15_4.py
+++ exercises/15_module_re/task_15_4.py
@@ -22,3 +22,46 @@ interface Loopback0
 
 Проверить работу функции на примере файла config_r1.txt.
 """
+
+import re
+def get_ints_without_description(filename):
+    result_no_desc = []
+    regex = r"interface (?P<intf>\S+)\n( .*\n)"
+    with open(filename,'r') as f:
+        result = ([match.groups() for match in re.finditer(regex,f.read())])
+        for line in result:
+            first,*last = line
+            if 'description' in ''.join(last):
+                continue
+            else:
+                result_no_desc.append(first)
+        return result_no_desc
+if __name__ == "__main__":
+    print(get_ints_without_description('config_r1.txt'))
+
+
+'''
+import re
+
+def get_ints_without_description(config):
+    
+    result = []
+    with open(config, 'r') as f:
+        for line in f:
+            match = re.match('interface \S+', line)
+            if match:
+                intf = str(match.group())
+                intf = intf.replace('interface','')
+                result.append(intf.strip())
+                continue
+            match = re.match(' description', line)
+            if match:  
+                result.pop(-1)
+                continue
+    return result
+    
+if __name__ == "__main__":
+    result = get_ints_without_description('config_r1.txt')
+    print (result)
+
+'''
